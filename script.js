d8effee2e8ddf6c0b8ed0aa64810388b1bc2dae5
jakimz-lgtm <stokkimz11@gmail.com>
2025-09-30 14:09:19 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-
+//1.Pilih tombol berdasarkan ID-nya
+cons tombolSapa=document.getElementByid('sapaButton');
+tombolSapa.addEventListener('click',function() {
+  alert('halo! Terimakasih sudah berkunjung');
+});
